9dfcda6b74de3977f310799d1431133616eee78f
manishkulkarni2003 <mk3304939@gmail.com>
2024-12-25 23:10:25 +0530

    Multer

--- backend/middlewares/multerMiddleware.js
+++ backend/middlewares/multerMiddleware.js
@@ -1,21 +1,22 @@
 import multer from "multer";
-import path from "path";
-import fs from "fs";
 
-// Ensure the temp folder exists
-const tempFolder = path.join(__dirname, "../public/temp");
-if (!fs.existsSync(tempFolder)) {
-    fs.mkdirSync(tempFolder, { recursive: true });
-}
-
-// Configuration for multer
+//configuration 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, tempFolder); // Use the absolute path to temp folder
+        cb(null, "./public/temp")
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname); // Retaining the original file name
-    }
-});
+        cb(null, file.originalname)
 
-export const upload = multer({ storage });
+    }
+})
+
+export const upload = multer({ storage, })
+
+
+
+
+
+
+
+
